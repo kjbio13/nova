8951c162e3f440dfcf9a241923889727a840b771
Kevin Souribio (Personal) <kevinsouribio@nandos-ltmb-kev.local>
2020-11-05 02:54:25 +0000

    added typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+interface AppProps {
+  sendSearchQuery(): void;
+}
+
+const App: React.FC<AppProps> = (sendSearchQuery) => {
+  const [searchValue, setSearchValue] = useState<string>();
+
+  const onSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const search = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={searchValue}
+        onChange={onSearchInput}
+        name="search"
+        type="text"
+      />
+      <button onClick={search}>Search</button>
     </div>
   );
-}
+};
 
 export default App;
